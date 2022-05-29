ac5cb316779b3a05134707429308b69a4b0646f9
Rafael Dos Santos Ferro <r.ferro@outlook.com>
2022-05-29 21:13:04 +0100

    First Code

---
+++ ExeAccountSLN/ExAccountService/Services/BudgetValidation.cs
@@ -0,0 +1,51 @@
+﻿namespace ExAccountService.Services
+{
+    public class BudgetValidationService
+    {
+        public List<Itens> getMaximumSpend(int[] listOne, int[] listTwo, int budget)
+        {
+            //Here we Have the validations of minimum requirements
+            if (listOne.Count() > 100 || listTwo.Count() > 100)
+                throw new Exception("List cannot be more than 100 intens!");
+
+            if (listOne.Count() == 0 || listTwo.Count() == 0)
+                throw new Exception("List cannot empty!");
+
+            if (budget <= 0)
+                throw new Exception("Budget is not enough to proceed!");
+
+            var possible1 = listOne.Where(x => x < budget);
+            var possible2 = listTwo.Where(x => x < budget);
+
+            if (possible1.Count() == 0 || possible2.Count() == 0)
+                throw new Exception("-1");
+
+            //Here we Have Logical to verify and set the possible options.
+            List<Itens> list = new List<Itens>();
+
+            foreach (var possible in possible1)
+            {
+                foreach (var insedePossible in possible2)
+                {
+                    if (possible + insedePossible <= budget)
+                        list.Add(new Itens
+                        {
+                            ValueIten1 = possible,
+                            ValueIten2 = insedePossible
+                        });
+                }
+            }
+
+            //Here we Have validation if it was possible to buy any item.
+            if (list.Count() == 0)
+                throw new Exception("-1");
+
+            return list;
+        }
+    }
+    public struct Itens
+    {
+        public int ValueIten1 { get; set; }
+        public int ValueIten2 { get; set; }
+    }
+}
---
+++ ExeAccountSLN/ExAccountTest/BudgetValidationTest.cs
@@ -0,0 +1,126 @@
+﻿using ExAccountService.Services;
+
+namespace ExAccountTest
+{
+    public class BudgetValidationTest
+    {
+        private BudgetValidationService budget;
+
+        public BudgetValidationTest()
+        {
+            budget = new BudgetValidationService();
+        }
+
+        [Fact]
+        public void SholdBeSuccesswith6()
+        {
+            int[] ListOne = new int[] { 1, 2, 3, 9 };
+            int[] ListTwo = new int[] { 3, 5, 6, 8, 4 };
+
+            var result = budget.getMaximumSpend(ListOne, ListTwo, 6);
+            var ResultList = new List<int>();
+            foreach (var res in result)
+            {
+                if (res.ValueIten1 + res.ValueIten2 > 6)
+                    ResultList.Add(res.ValueIten1 + res.ValueIten2);
+            }
+
+            Assert.Empty(ResultList);
+        }
+
+        [Fact]
+        public void SholdBeSuccesswith19()
+        {
+            int[] ListOne = new int[] { 1, 2, 3, 9 };
+            int[] ListTwo = new int[] { 3, 5, 6, 8, 4 };
+
+            var result = budget.getMaximumSpend(ListOne, ListTwo, 19);
+            var ResultList = new List<int>();
+            foreach (var res in result)
+            {
+                if (res.ValueIten1 + res.ValueIten2 > 19)
+                    ResultList.Add(res.ValueIten1 + res.ValueIten2);
+            }
+
+            Assert.Empty(ResultList);
+        }
+
+        [Fact]
+        public void SholdBeErrorMoreThan100Itens()
+        {
+            List<int> ListOne = new List<int> { 1, 2, 3, 9 };
+            List<int> ListTwo = new List<int> { 3, 5, 6, 8, 4 };
+
+            int a = 10;
+            while (ListOne.Count() <= 100)
+            {
+                ListOne.Add(a);
+                a++;
+            }
+            try
+            {
+                var result = budget.getMaximumSpend(ListOne.ToArray(), ListTwo.ToArray(), 6);
+                var ResultList = new List<int>();
+                Assert.Empty(result);
+            }
+            catch (Exception e)
+            {
+                Assert.Contains("List cannot be more than 100 intens!", e.Message);
+            }
+        }
+
+        [Fact]
+        public void SholdBeError0Itens()
+        {
+            List<int> ListOne = new List<int> { };
+            List<int> ListTwo = new List<int> { 3, 5, 6, 8, 4 };
+
+            try
+            {
+                var result = budget.getMaximumSpend(ListOne.ToArray(), ListTwo.ToArray(), 6);
+                var ResultList = new List<int>();
+                Assert.Empty(result);
+            }
+            catch (Exception e)
+            {
+                Assert.Contains("List cannot empty!", e.Message);
+            }
+        }
+
+        [Fact]
+        public void SholdBeErrorBudgetIsNotEnoughToProceed()
+        {
+            List<int> ListOne = new List<int> { 1, 2, 3, 9 };
+            List<int> ListTwo = new List<int> { 3, 5, 6, 8, 4 };
+
+            try
+            {
+                var result = budget.getMaximumSpend(ListOne.ToArray(), ListTwo.ToArray(), 0);
+                var ResultList = new List<int>();
+                Assert.Empty(result);
+            }
+            catch (Exception e)
+            {
+                Assert.Contains("Budget is not enough to proceed!", e.Message);
+            }
+        }
+
+        [Fact]
+        public void SholdBeErrorBudgetIsNotEnough()
+        {
+            List<int> ListOne = new List<int> { 1, 2, 3, 9 };
+            List<int> ListTwo = new List<int> { 3, 5, 6, 8, 4 };
+
+            try
+            {
+                var result = budget.getMaximumSpend(ListOne.ToArray(), ListTwo.ToArray(), 3);
+                var ResultList = new List<int>();
+                Assert.Empty(result);
+            }
+            catch (Exception e)
+            {
+                Assert.Contains("-1", e.Message);
+            }
+        }
+    }
+}
